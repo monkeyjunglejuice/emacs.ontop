c5315064f5fa8d760e2c52e4362882f6c49a059b
Dan Dee <monkeyjunglejuice@pm.me>
2024-04-16 12:25:11 +0800

    Activate paren-face per default

--- ontop-scheme.el
+++ ontop-scheme.el
@@ -139,11 +139,10 @@
 
 ;; … and/or make parens styleable, e.g. more or less prominent
 ;; <https://github.com/tarsius/paren-face>
-;; (use-package paren-face
-;;   :ensure t
-;;   :hook
-;;   ((scheme-mode inferior-scheme-mode geiser-repl-mode)
-;;    . paren-face-mode))
+(use-package paren-face
+  :ensure t
+  :hook
+  ((scheme-mode inferior-scheme-mode geiser-repl-mode) . paren-face-mode))
 
 ;;  ____________________________________________________________________________
 ;;; INDENTATION
