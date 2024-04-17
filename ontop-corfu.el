7b00928d1b4d290272eee91190b37859d809c570
Dan Dee <monkeyjunglejuice@pm.me>
2024-04-18 06:14:20 +0800

    Tune auto-completion, add Yasnippet and Cape

--- ontop-corfu.el
+++ ontop-corfu.el
@@ -20,16 +20,74 @@
 (use-package corfu
   :ensure t
   :init
-  ;; Enable Corfu globally. This is recommended since Dabbrev
-  ;; can be used globally (M-/). See also `corfu-excluded-modes'.
   (global-corfu-mode)
   (corfu-popupinfo-mode)
-  ;; Optional customizations
   :custom
-  ;; Enable auto completion
+  (corfu-cycle t)
   (corfu-auto t)
-  ;; Enable cycling for `corfu-next/previous'
-  (corfu-cycle t))
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.2)
+  (corfu-popupinfo-delay '(0.4 . 0.1))
+  (corfu-quit-at-boundary 'separator)
+  (corfu-quit-no-match 'separator)
+  (corfu-preview-current nil)
+  (corfu-preselect 'valid)
+  (corfu-on-exact-match 'nil)
+  :hook
+  ((eat-eshell-mode eshell-mode shell-mode) . corfu-mode)
+  :bind
+  (:map corfu-map
+        ;; ("TAB" . corfu-next)
+        ;; ([tab] . corfu-next)
+        ;; ("S-TAB" . corfu-previous)
+        ;; ([backtab] . corfu-previous)
+        ;; ("S-RET" . corfu-insert)
+        ;; ("S-<return>" . corfu-insert)
+        ("RET" . nil)))
+
+;;  ____________________________________________________________________________
+;;; CAPE
+;; <https://github.com/minad/cape>
+
+(use-package cape
+  :ensure t
+  :init
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  ;; Continuously update the candidates - deactivate if lagging
+  ;; (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
+  ;; ;; Compose own completion at point for Eglot
+  ;; (defun eon-eglot-capf-super ()
+  ;;   (setq-local completion-at-point-functions
+  ;;               (list (cape-capf-super
+  ;;                      #'eglot-completion-at-point
+  ;;                      #'cape-file))))
+  ;; :hook
+  ;; (eglot-managed-mode . #'eon-eglot-capf-super)
+  :bind
+  ("C-c p f" . cape-file))
+
+;;  ____________________________________________________________________________
+;;; YASNIPPET
+
+;; <https://github.com/joaotavora/yasnippet>
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :config
+  (add-to-list 'hippie-expand-try-functions-list #'yas-hippie-try-expand)
+  :hook
+  ((prog-mode text-mode) . yas-minor-mode))
+
+;; <https://github.com/AndreaCrotti/yasnippet-snippets>
+(use-package yasnippet-snippets
+  :ensure t)
+
+;; <https://github.com/elken/yasnippet-capf>
+;; (use-package yasnippet-capf
+;;   :ensure t
+;;   :after cape
+;;   :config
+;;   (add-to-list 'completion-at-point-functions #'yasnippet-capf))
 
 ;;  ____________________________________________________________________________
 ;;; EMACS (built-in)
@@ -38,31 +96,17 @@
 (use-package emacs
   :ensure nil
   :init
-  ;; TAB cycle if there are only few candidates
-  (setq completion-cycle-threshold 3)
   ;; Enable indentation + completion using the TAB key.
   ;; `completion-at-point' is often bound to M-TAB.
   (setq tab-always-indent 'complete))
 
 ;; Use Dabbrev with Corfu
-;; <https://www.gnu.org/software/emacs/manual/html_mono/emacs.html#Dynamic-Abbrevs>
 (use-package dabbrev
-  :ensure nil
-  ;; Swap M-/ and C-M-/
-  :bind (("M-/" . dabbrev-completion)
-         ("C-M-/" . dabbrev-expand))
   :config
   (add-to-list 'dabbrev-ignored-buffer-regexps "\\` ")
   ;; Since 29.1, use `dabbrev-ignored-buffer-regexps' on older.
   (add-to-list 'dabbrev-ignored-buffer-modes 'doc-view-mode)
   (add-to-list 'dabbrev-ignored-buffer-modes 'pdf-view-mode))
-
-(use-package eshell
-  :ensure nil
-  :hook
-  ;; Behave more like shell autocompletion
-  (eshell-mode . (lambda ()
-                   (setq-local corfu-auto nil))))
 
 ;;; ___________________________________________________________________________
 (provide 'ontop-corfu)
