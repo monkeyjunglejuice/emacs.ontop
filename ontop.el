2c2062f4fef1c1a5a8fd031d36a1dcf33a2e9fcc
Dan Dee <monkeyjunglejuice@pm.me>
2024-04-18 21:08:48 +0800

    v0.5.0

--- ontop.el
+++ ontop.el
@@ -19,7 +19,7 @@
 ;; Copyright (C) 2022-2024 Dan Dee
 ;; Author: Dan Dee <monkeyjunglejuice@pm.me>
 ;; URL: https://github.com/monkeyjunglejuice/emacs.ontop
-;; Version: 0.4.2
+;; Version: 0.5.0
 ;; Package-Requires: ((EMACS "28.2"))
 ;; Keywords: convenience
 ;; SPDX-License-Identifier: MIT
