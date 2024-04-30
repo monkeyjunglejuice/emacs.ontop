0bea6f1b064c59151170fd443ba22790963a802d
Dan Dee <monkeyjunglejuice@pm.me>
2024-04-30 19:32:09 +0800

    Rainbow-delimiters are not useful in comint-mode

--- ontop-core.el
+++ ontop-core.el
@@ -396,16 +396,14 @@
 (use-package rainbow-delimiters
   :ensure t
   :hook
-  ((prog-mode comint-mode eval-expression-minibuffer-setup)
-   . rainbow-delimiters-mode))
+  (prog-mode . rainbow-delimiters-mode))
 
 ;; … and/or make parens styleable, e.g. more or less prominent
 ;; <https://github.com/tarsius/paren-face>
 ;; (use-package paren-face
 ;;   :ensure t
 ;;   :hook
-;;   ((prog-mode comint-mode eval-expression-minibuffer-setup)
-;;    . paren-face-mode))
+;;   (prog-mode . paren-face-mode))
 
 ;;  ____________________________________________________________________________
 ;; INDENTATION
