42a11e27e657322085cadf4ca387dfdb3c00fee8
Dan Dee <monkeyjunglejuice@pm.me>
2025-10-15 13:44:39 +0800

    Commentary header

--- eon-corfu.el
+++ eon-corfu.el
@@ -1,4 +1,4 @@
-;;; eon-corfu.el --- Code (auto) completion -*- lexical-binding: t; no-byte-compile: t; -*-
+;;; eon-corfu.el --- Code (auto)completion -*- lexical-binding: t; no-byte-compile: t; -*-
 ;; This file is part of Emacs ONTOP
 ;; https://github.com/monkeyjunglejuice/emacs.ontop
 
