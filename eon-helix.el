562a77a694263d4399ad82307f2f00336b47d3cd
Dan Dee <monkeyjunglejuice@pm.me>
2025-10-15 13:38:17 +0800

    Use the fresh version from main branch

--- eon-helix.el
+++ eon-helix.el
@@ -10,10 +10,51 @@
 ;;; HELIX
 ;; <https://github.com/mgmarlow/helix-mode>
 
-(use-package helix :ensure t
+(use-package helix
+  :vc (:url "https://github.com/mgmarlow/helix-mode")
   :diminish helix-normal-mode helix-insert-mode
+
+  :init
+  (defun eon-helix--bind-leader-in-states (old new)
+    "Explicitly bind the leader key to prevent hijacking."
+    (when (and (stringp new) (> (length new) 0))
+      (dolist (m (list helix-normal-state-keymap
+                       helix-view-map))
+        (when (and (stringp old) (> (length old) 0))
+          (define-key m (kbd old) nil))
+        (define-key m (kbd new) ctl-z-map))))
+
+  (defun eon-helix--set-leaders (sym val)
+    "Setter for leader and local leader keys.
+Used by custom variables `eon-helix-leader-key' and `eon-helix-localleader-key'."
+    (let ((old (and (boundp sym) (default-value sym))))
+      (set-default sym val)
+      (with-eval-after-load 'helix
+        (pcase sym
+          ('eon-helix-leader-key
+           (eon-helix--bind-leader-in-states old val))
+          ('eon-helix-localleader-key
+           (when old (define-key ctl-z-map (kbd old) nil))
+           (define-key ctl-z-map (kbd val)
+                       (cons "Local" ctl-z-localleader-map)))))))
+
+  (defcustom eon-helix-leader-key ","
+    "Leader key for Helix."
+    :group 'eon :type 'string
+    :set #'eon-helix--set-leaders
+    :initialize 'custom-initialize-set)
+
+  (defcustom eon-helix-localleader-key ","
+    "Local leader key for Helix."
+    :group 'eon :type 'string
+    :set #'eon-helix--set-leaders
+    :initialize 'custom-initialize-set)
+
   :config
-  (helix-mode))
+  (helix-mode)
+
+  ;; Explicitly bind the leader key
+  (eon-helix--bind-leader-in-states nil eon-helix-leader-key))
 
 ;; KEYS
 ;; You can add new keys to the Helix keymaps via helix-define-key:
