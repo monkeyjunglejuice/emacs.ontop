af677ba74b8a4a6b535d0dddc492edaa55fdb57e
Dan Dee <monkeyjunglejuice@pm.me>
2025-10-18 13:16:07 +0800

    Add "The Matrix" theme

---
+++ eon-theme-matrix.el
@@ -0,0 +1,23 @@
+;;; eon-theme-matrix.el --- The Matrix theme -*- lexical-binding: t; no-byte-compile: t; -*-
+;; This file is part of Emacs ONTOP
+;; https://github.com/monkeyjunglejuice/emacs.ontop
+
+;;; Commentary:
+;;
+;;; Code:
+
+;;  ____________________________________________________________________________
+;;; THE MATRIX THEME
+;; <https://github.com/monkeyjunglejuice/matrix-emacs-theme>
+
+(use-package the-matrix-theme :ensure t)
+
+;; Example how to set "The Matrix" theme in your init.el:
+;; (with-eval-after-load 'eon-theme-matrix
+;;   (setopt eon-theme-dark 'the-matrix
+;;           eon-theme-variant-default 'dark)
+;;   (eon-theme-load-default))
+
+;;  ____________________________________________________________________________
+(provide 'eon-theme-matrix)
+;;; eon-theme-matrix.el ends here
