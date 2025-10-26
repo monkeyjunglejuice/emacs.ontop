1e4ed2d19006b17f91eb22604ca351c3a6d5d2e3
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-10-26 20:18:10 +0800

    Current Corfu config, still not working perfectly

--- eon-corfu.el
+++ eon-corfu.el
@@ -17,8 +17,34 @@
   (setopt completion-auto-help nil)
   :custom
   (corfu-auto t)
+  (corfu-auto-delay 0.2)
   (corfu-auto-prefix 2)
+  (corfu-cycle t)
+  (corfu-max-width 120)
+  (corfu-on-exact-match 'nil)
+  (corfu-popupinfo-delay '(0.3 . 0.1))
+  (corfu-popupinfo-max-height 10)
+  (corfu-preselect 'valid)
+  (corfu-preview-current nil)
+  (corfu-quit-at-boundary nil)
+  (corfu-quit-no-match 'separator)
+  (global-corfu-minibuffer t)
   :config
+  (global-corfu-mode)
+  (corfu-echo-mode)
+  :bind
+  (:map corfu-map
+        ;; ("TAB" . corfu-next)
+        ;; ([tab] . corfu-next)
+        ;; ("S-TAB" . corfu-previous)
+        ;; ([backtab] . corfu-previous)
+        ;; ("S-RET" . corfu-insert)
+        ;; ("S-<return>" . corfu-insert)
+        ("RET" . nil)))
+
+(use-package corfu-history :ensure nil
+  :config
+  (eon-add-to-list-setopt 'savehist-additional-variables 'corfu-history)
   :hook
   (corfu-mode . corfu-history-mode))
 
