8838991d5a5e6b7bd274089e0b1da24da31b1269
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-10-27 09:51:02 +0800

    Update module list

--- eon-setup-modules.el
+++ eon-setup-modules.el
@@ -4,18 +4,18 @@
 
 ;;; Commentary:
 ;;
-;;; Code:
-
-;;  ____________________________________________________________________________
-;;; SETUP
-;;
-;; --> Enable or disable modules below via commenting and uncommenting ...
+;; --> Enable or disable modules below by uncommenting and commenting ...
 ;;     ... then apply your changes via "M-x eon-require-modules".
 ;;     You can also load single modules via "M-x eon-load-module RET eon-"
 ;;     and unload manually loaded modules via "M-x eon-unload-module RET eon-"
 ;;
 ;; --> Documentation lives in the module files. To learn more, place the cursor
 ;;     on a module symbol below and do "<leader> f o" to visit the file.
+;;
+;;; Code:
+
+;;  ____________________________________________________________________________
+;;; AVAILABLE MODULES
 
 (setopt
  eon-modules
@@ -41,29 +41,31 @@
    ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    ;;; THEMES
 
-   eon-theme-catppuccin         ; Soothing pastel themes
-   eon-theme-doom               ; Doom Emacs theme pack
-   eon-theme-spacemacs          ; Spacemacs theme
-   eon-theme-matrix             ; Inspired by "The Matrix" movie
+   ;; eon-theme-catppuccin         ; Soothing pastel themes
+   ;; eon-theme-doom               ; Doom Emacs theme pack
+   ;; eon-theme-spacemacs          ; Spacemacs theme
+   ;; eon-theme-matrix             ; Inspired by "The Matrix" movie
 
    ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    ;;; OPTIONAL MODULES
 
+   ;; eon-autoindent                ; Indent code immediately
    ;; eon-autoupdate                ; Update packages automatically
+   eon-cape                      ; Completion-at-point extensions
    eon-consult                   ; Navigation and search framework
    eon-corfu                     ; Code (auto)completion
    ;; eon-embark                    ; Minibuffer actions and context menu
-   eon-flycheck                  ; Syntax checker alternative
+   ;; eon-flycheck                  ; Syntax checker alternative
    ;; eon-fonts                     ; Curated font sets
    eon-helpful                   ; Extended help viewer
-   eon-git                       ; Git user interface inside Emacs
+   eon-git                       ; Git user interface
    eon-marginalia                ; Rich annotations
-   ;; eon-pdftools                  ; PDF reader and editor
+   ;; eon-pdftools                  ; Sophisticated PDF tool suite
    ;; eon-smartparens               ; Edit parenthesis structurally
    ;; eon-snippets                  ; Code snippets
    eon-switchwindow              ; Window management
    eon-todo                      ; Highlight todo keywords in comments
-   eon-vertico                   ; Versatile vertical completion
+   eon-vertico                   ; Versatile vertical completion UI
 
    ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    ;;; PROGRAMMING LANGUAGES
@@ -93,10 +95,10 @@
    eon-lang-web                  ; HTML/CSS editing
 
    ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   ;;; AI TOOLS
+   ;;; AI
 
-   ;; eon-ollama                    ; Base config to support local LLMs
-   ;; eon-gptel                     ; Gptel LLM chat client
+   ;; eon-ai                        ; Shared config for AI integration
+   ;; eon-gptel                     ; Comprehensive AI Chat client
 
    ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
    ;;; PERSONAL MODULES
