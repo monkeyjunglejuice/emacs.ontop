b122d173f5e6a55521da4e14d69d3737c71d147f
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-10-29 11:51:42 +0800

    Formatting/Commentary

--- eon-setup-personal.el
+++ eon-setup-personal.el
@@ -1,10 +1,11 @@
 ;;; eon-setup-personal.el --- Personal Emacs Lisp -*- lexical-binding: t; no-byte-compile: t; -*-
 
 ;;; Commentary:
+;;
 ;; The code within this file will load last, so that you can shadow or override
 ;; any setting that has been defined by Emacs ONBOARD and Emacs ONTOP.
 ;;
-;; You can also load other Elisp files from here
+;; You can also load other Elisp files from here via:
 ;; (load-file (expand-file-name "~/path/to/another-file.el"))
 ;;
 ;; Or you can load other Elisp files from your init.el after this file/module
