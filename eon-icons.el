32e88f660b7a178bb6154019b7e987f2b3274dde
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-11-03 20:13:40 +0800

    Support icons almost everywhere

---
+++ eon-icons.el
@@ -0,0 +1,44 @@
+;;; eon-icons.el --- Icons everywhere -*- lexical-binding: t; no-byte-compile: t; -*-
+;; This file is part of Emacs ONTOP
+;; https://github.com/monkeyjunglejuice/emacs.ontop
+
+;;; Commentary:
+;;
+;;; Code:
+
+;; _____________________________________________________________________________
+;;; ICONS
+
+(use-package nerd-icons :ensure t)
+
+;; Dired
+(use-package nerd-icons-dired :ensure t
+  :hook
+  (dired-mode . nerd-icons-dired-mode))
+
+;; Ibuffer
+(use-package nerd-icons-ibuffer :ensure t
+  :hook
+  (ibuffer-mode . nerd-icons-ibuffer-mode))
+
+;; Completion
+(use-package nerd-icons-completion :ensure t
+  :config
+  (nerd-icons-completion-mode))
+
+;; Marginalia
+(when (eon-modulep 'eon-marginalia)
+  (use-package nerd-icons-completion :ensure t
+    :hook
+    (marginalia-mode #'nerd-icons-completion-marginalia-setup)))
+
+;; Corfu
+(when (eon-modulep 'eon-corfu)
+  (use-package nerd-icons-corfu :ensure t
+    :config
+    (eon-add-to-list-setopt 'corfu-margin-formatters
+                            #'nerd-icons-corfu-formatter)))
+
+;; _____________________________________________________________________________
+(provide 'eon-icons)
+;;; eon-icons.el ends here
