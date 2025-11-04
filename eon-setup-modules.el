a28559bf2e76b672b15c80f5e8e9ad9f7beb08fe
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-11-04 22:48:15 +0800

    General update; modules renamed; formatting/styling

--- eon-setup-modules.el
+++ eon-setup-modules.el
@@ -19,56 +19,57 @@
 
 (setopt
  eon-modules
- '(;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ '(;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
    ;;; MODULES TO LOAD EARLY
 
    ;; eon-compileangel              ; Improved native compilation
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
    ;;; NECESSARY MODULES
 
    eon                           ; The Emacs ONBOARD starter-kit
-   eon-core                      ; Required shared settings and definitions
+   eon-base                      ; Shared packages and definitions
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   ;;; KEYBINDINGS -- if none selected, fall back to Emacs keybindings
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+   ;;; MODAL EDITING - if none selected, fall back to Emacs keybindings
 
-   ;; eon-evil                      ; Modular editing, Vim keybindings
-   ;; eon-god                       ; Modular editing, Emacs keybindings
-   ;; eon-helix                     ; Modular editing, Helix keybindings
-   ;; eon-meow                      ; Modular editing, Meow keybindings
+   ;; eon-evil                      ; Modal editing: Vim keybindings
+   ;; eon-god                       ; Modal editing: Emacs keybindings
+   ;; eon-helix                     ; Modal editing: Helix keybindings
+   ;; eon-meow                      ; Modal editing: Meow keybindings
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
    ;;; THEMES
 
    ;; eon-theme-catppuccin         ; Soothing pastel themes
    ;; eon-theme-doom               ; Doom Emacs theme pack
-   ;; eon-theme-spacemacs          ; Spacemacs theme
-   ;; eon-theme-matrix             ; Inspired by "The Matrix" movie
+   ;; eon-theme-spacemacs          ; Spacemacs themes
+   ;; eon-theme-matrix             ; Theme inspired by "The Matrix" movie
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
    ;;; OPTIONAL MODULES
 
    eon-activities                ; Suspend/resume windows, buffers, etc.
-   ;; eon-autoindent                ; Indent code immediately
    ;; eon-autoupdate                ; Update packages automatically
    eon-cape                      ; Completion-at-point extensions
-   eon-consult                   ; Navigation and search framework
-   eon-corfu                     ; Code (auto)completion
+   eon-consult                   ; Navigation and search
+   ;; eon-corfu                     ; Code (auto-)completion
    ;; eon-embark                    ; Minibuffer actions and context menu
    ;; eon-flycheck                  ; Syntax checker alternative
    ;; eon-fonts                     ; Curated font sets
+   eon-git                       ; Magit user interface and friends
    eon-helpful                   ; Extended help viewer
-   eon-git                       ; Git user interface
+   ;; eon-icons                     ; Icons everywhere
+   ;; eon-indent                    ; Indent code immediately
    eon-marginalia                ; Rich annotations
    ;; eon-pdftools                  ; Sophisticated PDF tool suite
    ;; eon-smartparens               ; Edit parenthesis structurally
    ;; eon-snippets                  ; Code snippets
-   eon-switchwindow              ; Window management
+   eon-switchwindow              ; Navigate windows
    eon-todo                      ; Highlight todo keywords in comments
-   eon-vertico                   ; Versatile vertical completion UI
+   eon-vertico                   ; Vertical minibuffer completion UI
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
    ;;; PROGRAMMING LANGUAGES
 
    ;; eon-lang-clojure              ; Clojure and ClojureScript programming
@@ -82,27 +83,27 @@
    ;; eon-lang-lua                  ; Lua programming
    ;; eon-lang-ocaml                ; Ocaml programming
    ;; eon-lang-racket               ; Racket programming
-   ;; eon-lang-scheme               ; Scheme programming with Geiser
-   ;; eon-lang-scheme-chez          ; Chez Scheme support
-   ;; eon-lang-scheme-chibi         ; Chibi Scheme support
-   ;; eon-lang-scheme-chicken       ; Chicken Scheme support
-   ;; eon-lang-scheme-gambit        ; Gambit Scheme support
-   ;; eon-lang-scheme-gauche        ; Gauche Scheme support
-   ;; eon-lang-scheme-guile         ; Guile Scheme support
-   ;; eon-lang-scheme-kawa          ; Kawa Scheme support
-   ;; eon-lang-scheme-mit           ; MIT Scheme support
+   ;; eon-lang-scheme               ; Shared config for Scheme programming
+   ;; eon-lang-scheme-chez          ; Chez Scheme
+   ;; eon-lang-scheme-chibi         ; Chibi Scheme
+   ;; eon-lang-scheme-chicken       ; Chicken Scheme
+   ;; eon-lang-scheme-gambit        ; Gambit Scheme
+   ;; eon-lang-scheme-gauche        ; Gauche Scheme
+   ;; eon-lang-scheme-guile         ; Guile Scheme
+   ;; eon-lang-scheme-kawa          ; Kawa Scheme
+   ;; eon-lang-scheme-mit           ; MIT Scheme
    ;; eon-lang-scheme-racket        ; consider `eon-lang-racket' instead
-   ;; eon-lang-scheme-stklos        ; Stklos Scheme support
+   ;; eon-lang-scheme-stklos        ; Stklos Scheme
    eon-lang-web                  ; HTML/CSS editing
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   ;;; AI
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+   ;;; AI / LLM
 
-   ;; eon-ai                        ; Shared config for AI integration
-   ;; eon-gptel                     ; Comprehensive AI Chat client
+   ;; eon-ai                        ; Shared functionality for AI integration
+   ;; eon-gptel                     ; Comprehensive AI chat client
 
-   ;; - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   ;;; PERSONAL MODULES
+   ;; . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+   ;;; PERSONAL MODULE
 
    ;; Below is an empty extra module that runs your personal Elisp code.
    ;; Your code will load last, so that you can override or shadow
