63ecf2834e777052fc0c811293b23d0154b861ef
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-11-17 17:17:28 +0800

    Version 1.3.2

--- eon-init.el
+++ eon-init.el
@@ -18,7 +18,7 @@
 ;; Copyright (C) 2022-2025 Dan Dee
 ;; Author: Dan Dee <monkeyjunglejuice@pm.me>
 ;; URL: https://github.com/monkeyjunglejuice/emacs.ontop
-;; Version: 1.3.1
+;; Version: 1.3.2
 ;; Package-Requires: ((emacs "30.1"))
 ;; Keywords: convenience
 ;; SPDX-License-Identifier: MIT
