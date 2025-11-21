6633b43635e7c54b329654c690eb38cdcc5ecf4a
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-11-21 10:01:29 +0800

    Improve commentary

--- modules/eon-lang-erlang.el
+++ modules/eon-lang-erlang.el
@@ -27,7 +27,7 @@
 
 (use-package eglot :ensure nil
   :custom
-  ;; A longer timeout seems required for the first run in a new project
+  ;; A longer timeout may be required for the first run in a new project
   (eglot-connect-timeout 60)  ; default: 30
   :config
   ;; Make sure to adapt the path and use the .bat script for Windows
