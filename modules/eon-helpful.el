b33298a51642b373d411affe44ac2a52275d8407
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2025-11-23 20:01:32 +0800

    Formatting

--- modules/eon-helpful.el
+++ modules/eon-helpful.el
@@ -15,20 +15,25 @@
 
 (use-package helpful :ensure t
   :defer t
+
   :bind
+
   ;; Lookup the current symbol at point
   ("C-h o" . #'helpful-at-point)
+
   ;; Look up functions and macros
   ;; Note that the built-in `describe-function' includes both functions
   ;; and macros. `helpful-function' is functions only, so we provide
   ;; `helpful-callable' as a drop-in replacement.
   ("C-h f" . #'helpful-callable)
+
   ;; Look up functions, exclude macros
   ;; By default, C-h F is bound to `info-goto-emacs-command-node'. Helpful
   ;; already links to the manual, if a function is referenced there.
   ("C-h F" . #'helpful-function)
   ("C-h v" . #'helpful-variable)
   ("C-h k" . #'helpful-key)
+
   (:map ctl-z-h-map
         ("c" . #'helpful-command)
         ("f" . #'helpful-callable)
@@ -36,6 +41,7 @@
         ("o" . #'helpful-at-point)
         ("k" . #'helpful-key)
         ("v" . #'helpful-variable))
+
   (:map eon-localleader-elisp-map
         ("h" . #'helpful-symbol)))
 
