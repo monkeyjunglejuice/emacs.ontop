7e9c56598e712fc3f161607c2a3d85428147fb7d
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-01-03 18:53:13 +0800

    Corfu setup: completion on TAB

--- modules/eon-corfu.el
+++ modules/eon-corfu.el
@@ -17,43 +17,48 @@
   ;; Prevent *Completions* buffer from popping up
   (setopt completion-auto-help nil)
 
+  ;; Enable Corfu globally
+  (global-corfu-mode)
+
   :custom
 
   (corfu-auto nil)
-  (corfu-auto-delay 0.2)
+  (corfu-auto-delay 0.3)
   (corfu-auto-prefix 2)
   (corfu-cycle t)
   (corfu-max-width 120)
   (corfu-on-exact-match 'nil)
   (corfu-popupinfo-delay '(0.3 . 0.1))
-  (corfu-popupinfo-max-height 10)
+  (corfu-popupinfo-max-height 12)
   (corfu-preselect 'valid)
   (corfu-preview-current nil)
   (corfu-quit-at-boundary 'separator)
   (corfu-quit-no-match 'separator)
   (global-corfu-minibuffer t)
 
-  :config
-
-  (global-corfu-mode)
-  (corfu-echo-mode)
-
   :bind
 
   (:map corfu-map
         ;; ("TAB" . corfu-next)
-        ;; ([tab] . corfu-next)
+        ([tab] . corfu-next)
         ;; ("S-TAB" . corfu-previous)
-        ;; ([backtab] . corfu-previous)
+        ([backtab] . corfu-previous)
         ;; ("S-RET" . corfu-insert)
         ;; ("S-<return>" . corfu-insert)
-        ("RET" . nil)))
+        ;; ("RET" . nil)
+        ))
 
+;; Update Corfu history and sort completions by history
 (use-package corfu-history :ensure nil
   :config
   (eon-add-to-list* 'savehist-additional-variables 'corfu-history)
   :hook
   (corfu-mode . corfu-history-mode))
+
+;; Show candidate documentation in the echo area
+(use-package corfu-echo :ensure nil
+  :hook
+  (corfu-mode . corfu-echo-mode))
 
 ;; _____________________________________________________________________________
 (provide 'eon-corfu)
