6c90eb6d5a5cc66bea3b95c6faf420edd4891254
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-01-04 17:46:59 +0800

    Clean up and refine config

--- modules/eon-corfu.el
+++ modules/eon-corfu.el
@@ -9,32 +9,36 @@
 ;; _____________________________________________________________________________
 ;;; CORFU
 ;; <https://github.com/minad/corfu>
+;; <https://github.com/minad/corfu/wiki>
 ;; <https://github.com/minad/corfu?tab=readme-ov-file#orderless-completion>
 
 (use-package corfu :ensure t
   :init
 
   ;; Prevent *Completions* buffer from popping up
-  (setopt completion-auto-help nil)
+  ;; (setopt completion-auto-help nil)
 
   ;; Enable Corfu globally
   (global-corfu-mode)
 
   :custom
 
-  (corfu-auto nil)
-  (corfu-auto-delay 0.3)
+  ;; Autocompletion on?
+  (corfu-auto t)
   (corfu-auto-prefix 2)
+
+  ;; Enable Corfu completion in the minibuffer too?
+  (global-corfu-minibuffer t)
+
   (corfu-cycle t)
+  (corfu-count 12)
   (corfu-max-width 120)
-  (corfu-on-exact-match 'nil)
-  (corfu-popupinfo-delay '(0.3 . 0.1))
-  (corfu-popupinfo-max-height 12)
+  (corfu-scroll-margin 1)
   (corfu-preselect 'valid)
   (corfu-preview-current nil)
+  (corfu-on-exact-match 'nil)
   (corfu-quit-at-boundary 'separator)
   (corfu-quit-no-match 'separator)
-  (global-corfu-minibuffer t)
 
   :bind
 
@@ -55,10 +59,21 @@
   :hook
   (corfu-mode . corfu-history-mode))
 
-;; Show candidate documentation in the echo area
+;; Show candidate autodoc in the echo area
 (use-package corfu-echo :ensure nil
+  :custom
+  (corfu-echo-delay '(0.5 . 0.2))
   :hook
   (corfu-mode . corfu-echo-mode))
+
+;; To display documentation in a popup overlay only via "M-h" on demand,
+;; it's initial timer is set to 60 seconds.
+(use-package corfu-popupinfo :ensure nil
+  :custom
+  (corfu-popupinfo-delay '(60.0 . 0.1))
+  (corfu-popupinfo-max-height 10)
+  :hook
+  (corfu-mode . corfu-popupinfo-mode))
 
 ;; _____________________________________________________________________________
 (provide 'eon-corfu)
