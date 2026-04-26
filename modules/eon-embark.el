990c1830f48878704523cda940bf15ffd7636f8a
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-04-26 13:04:03 +0800

    Formatting

--- modules/eon-embark.el
+++ modules/eon-embark.el
@@ -25,16 +25,22 @@
 ;; <https://github.com/oantolin/embark/wiki>
 
 (use-package embark :ensure t
+
   :init
+
   ;; Optionally replace the key help with a completing-read interface
   (setq prefix-help-command #'embark-prefix-help-command)
+
   :config
+
   ;; Hide the mode line of the Embark live/completions buffers
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                  nil
                  (window-parameters (mode-line-format . none))))
+
   :bind
+
   ("C-."   . embark-act)
   ("C-;"   . embark-dwim)
   ("C-h B" . embark-bindings))
