6d55947df07914ebb2c629e078771cc983070729
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-05-11 14:54:24 +0800

    Just formatting

--- modules/eon-compileangel.el
+++ modules/eon-compileangel.el
@@ -40,7 +40,9 @@
 
 (use-package compile-angel :ensure t
   :demand t
+
   :config
+
   ;; The following directive prevents compile-angel from compiling your init
   ;; files. If you choose to remove this push to `compile-angel-excluded-files'
   ;; and compile your pre/post-init files, ensure you understand the
@@ -50,6 +52,7 @@
   ;; at the top of your init file.
   (push "/init.el" compile-angel-excluded-files)
   (push "/early-init.el" compile-angel-excluded-files)
+
   ;; Exclude the custom-file, recentf, and savehist files
   ;; Ensure that compile-angel is loaded using `require`, `use-package`, or
   ;; another package manager, as compile-angel-excluded-files is declared after
@@ -58,19 +61,23 @@
   (with-eval-after-load "savehist"
     (push (concat "/" (file-name-nondirectory savehist-file))
           compile-angel-excluded-files))
+
   ;; Ensure that the value of `recentf-save-file` is updated before proceeding
   (with-eval-after-load "recentf"
     (push (concat "/" (file-name-nondirectory recentf-save-file))
           compile-angel-excluded-files))
+
   ;; Ensure that the value of `custom-file` is updated before proceeding
   (with-eval-after-load "cus-edit"
     (when (stringp custom-file)
       (push (concat "/" (file-name-nondirectory custom-file))
             compile-angel-excluded-files)))
+
   ;; Enable the (compile-angel-on-load-mode) mode after the above
   ;; Set `compile-angel-verbose' to nil to disable compile-angel messages.
   ;; (When set to nil, compile-angel won't show which file is being compiled.)
   (setq compile-angel-verbose t)
+
   ;; Compile automatically when an Elisp file is saved?
   ;; (add-hook 'emacs-lisp-mode-hook #'compile-angel-on-save-local-mode)
   ;; (setq compile-angel-display-buffer t)
