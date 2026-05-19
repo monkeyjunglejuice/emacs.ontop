5b09a89b0b37b608c1722247fde25f1a01cfc890
Dan Dee <64652122+monkeyjunglejuice@users.noreply.github.com>
2026-05-19 17:11:00 +0800

    v2.0.2

--- modules/eon-switchwindow.el
+++ modules/eon-switchwindow.el
@@ -1,6 +1,6 @@
 ;;; eon-switchwindow.el --- Navigate windows -*- lexical-binding: t; no-byte-compile: t; -*-
 
-;; Version: 2.0.1
+;; Version: 2.0.2
 ;; URL: https://github.com/monkeyjunglejuice/emacs.ontop
 ;; Package-Requires: ((emacs "30.1")
 ;;                    (use-package "2.4.6"))
